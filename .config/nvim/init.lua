fca9eb6aac0a37f630bc8b4be8253a2adf1fd69c
Roshan Mishra <roshanmishrarinem@gmail.com>
2024-03-08 22:11:14 +0530

    fix: gitsigns keymaps not working

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -163,24 +163,6 @@ require('lazy').setup {
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim',    opts = {} },
 
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following lua:
-  --    require('gitsigns').setup({ ... })
-  --
-  -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-
   -- NOTE: Plugins can also be configured to run lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
@@ -828,6 +810,7 @@ require('lazy').setup {
   require 'rinem.plugins.oil',
   require 'rinem.plugins.neoscroll',
   require 'rinem.plugins.fidget',
+  require 'rinem.plugins.gitsigns',
   -- require 'rinem.plugins.lsp-zero',
   require 'rinem.plugins.triptych',
 
